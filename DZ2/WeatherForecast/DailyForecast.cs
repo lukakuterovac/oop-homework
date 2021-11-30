76a862408f2c2b4a5c53f01864f5612b0fa863f3
LukaKuterovac <luka.kuterovac@gmail.com>
2021-11-30 20:38:04 +0100

    Added Weather,DailyWeather,WeeklyWeather and ForecastUtilities classes to WeatherForecast class library. Added WeatherApplication to ConsoleWeatherUI console application.

---
+++ DZ2/WeatherForecast/DailyForecast.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherForecast
+{
+    public class DailyForecast
+    {
+        private DateTime date;
+        private Weather forecast;
+
+        public DateTime Date
+        {
+            get { return this.date; }
+        }
+        public Weather Forecast
+        {
+            get { return this.forecast; }
+        }
+        public double Temperature
+        {
+            get { return this.forecast.Temperature; }
+        }
+        public double Humidity
+        {
+            get { return this.forecast.Humidity; }
+        }
+        public double WindSpeed
+        {
+            get { return this.forecast.WindSpeed; }
+        }
+
+        public DailyForecast(DailyForecast dailyForecast)
+        {
+            this.date = dailyForecast.Date;
+            this.forecast = dailyForecast.Forecast;
+        }
+        public DailyForecast(DateTime date, Weather forecast)
+        {
+            this.date = date;
+            this.forecast = forecast;
+        }
+
+        public static bool operator >(DailyForecast left, DailyForecast right)
+        {
+            if (left.Temperature > right.Temperature)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator <(DailyForecast left, DailyForecast right)
+        {
+            if (left.Temperature > right.Temperature)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string GetAsString()
+        {
+            return $"{date}: {forecast.GetAsString()}";
+        }
+    }
+}
---
+++ DZ2/WeatherForecast/WeeklyForecast.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherForecast
+{
+    public class WeeklyForecast
+    {
+        private DailyForecast[] weeklyForecast;
+
+        public WeeklyForecast(DailyForecast[] weeklyForecast)
+        {
+            this.weeklyForecast = weeklyForecast;
+        }
+
+        public DailyForecast this[int index]
+        {
+            get { return this.weeklyForecast[index]; }
+        }
+
+        public double GetMaxTemperature()
+        {
+            DailyForecast maxTemperature = weeklyForecast[0];
+            for (int i = 0; i < 7; i++)
+            {
+                if (weeklyForecast[i] > maxTemperature)
+                {
+                    maxTemperature = weeklyForecast[i];
+                }
+            }
+            return maxTemperature.Temperature;
+        }
+
+        public string GetAsString()
+        {
+            return $"{weeklyForecast[0].GetAsString()}\n{weeklyForecast[1].GetAsString()}\n{weeklyForecast[2].GetAsString()}\n{weeklyForecast[3].GetAsString()}\n{weeklyForecast[4].GetAsString()}\n{weeklyForecast[5].GetAsString()}\n{weeklyForecast[6].GetAsString()}\n";
+        }
+    }
+}
